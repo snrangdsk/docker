1f1c7496300df98f27716396fbbb3404f2166da8
root <root@ip-172-31-24-72.ap-northeast-2.compute.internal>
2019-07-10 15:47:46 +0000

    from feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed again!!!
+          this change was from branch feature
         </p>
         <a
           className="App-link"
